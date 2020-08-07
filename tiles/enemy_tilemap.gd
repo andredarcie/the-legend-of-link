e8886f7f5d21825500d39f4571ba0aa278865ac1
andredarcie <andrendarcie@gmail.com>
2020-08-06 21:54:28 -0300

    Enemy Placer

---
+++ tiles/enemy_tilemap.gd
@@ -0,0 +1,12 @@
+extends TileMap
+
+func _ready():
+	var size = get_cell_size()
+	var offset = size / 2
+	
+	for tile in get_used_cells():
+		var name = get_tileset().tile_get_name(get_cell(tile.x, tile.y))
+		var node = load(str('res://enemies/', name, '.tscn')).instance()
+		node.global_position = tile * size + offset
+		get_parent().call_deferred('add_child', node)
+	queue_free()

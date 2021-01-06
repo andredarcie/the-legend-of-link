1c275d4941d26b6c594ab7ac4fbdfcc994ec51ba
andredarcie <andrendarcie@gmail.com>
2021-01-05 22:30:11 -0300

    New deploy

--- npcs/mage/Mage.gd
+++ npcs/mage/Mage.gd
@@ -2,16 +2,24 @@ class_name Mage extends Node2D
 
 var dialogBox: DialogBox = null
 var characterName = 'Old Mage'
-var dialogs = ["Welcome to this incredible adventure!","This is the way."]
+var dialogs = ["Desista agora!","Mas se não for desistir siga esse [wave]caminho."]
+var talkAboutTheKey = false
 
 func _ready():
 	dialogBox = get_tree().get_root().find_node("DialogBox", true, false) as DialogBox
 
 func _on_Area2D_body_entered(body):
+	if self.talkAboutTheKey:
+		dialogBox.start_dialog(self.characterName, [
+			'Sentido-se perdido?'
+		])
+		return
+		
 	if (body as Player).keys > 0:
 		dialogBox.start_dialog(self.characterName, [
-			'Haha! You got the key!'
+			'Haha! Você encontrou a maldita chave.'
 		])
+		self.talkAboutTheKey = true
 		return
 		
 	dialogBox.start_dialog(self.characterName, self.dialogs)

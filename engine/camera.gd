b845011e0e25a307b74de0d4c752a6f4833df358
andredarcie <andrendarcie@gmail.com>
2021-01-17 11:28:08 -0300

    New screen resolution

--- engine/camera.gd
+++ engine/camera.gd
@@ -7,8 +7,8 @@ func _ready():
 	
 func _process(delta):
 	var pos = get_node('../player').global_position - Vector2(0, 16)
-	var x = floor(pos.x / 256) * 256
-	var y = floor(pos.y / 224) * 224
+	var x = floor(pos.x / 320) * 320
+	var y = floor(pos.y / 180) * 180
 	global_position = Vector2(x, y)
 
 func body_entered(body):

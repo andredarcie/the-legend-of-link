ace0dde4521836a3e8393c1496bdf958f2970e36
andredarcie <andrendarcie@gmail.com>
2021-01-19 21:35:39 -0300

    Bat enemy

---
+++ enemies/Bat/Bat.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+onready var base_enemy: BaseEnemy = $BaseEnemy
+onready var texture = load("res://enemies/Bat/bat.png")
+onready var hurt_texture = load("res://enemies/Bat/bat_hurt.png")
+
+func _ready():
+	base_enemy.set_can_see(false)
+	base_enemy.set_texture(texture)
+	base_enemy.set_hurt_texture(hurt_texture)
+	base_enemy.set_max_health(1)

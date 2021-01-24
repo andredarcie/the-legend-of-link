b9aa19891d01fc8756b19aae9de8288362b093af
andredarcie <andrendarcie@gmail.com>
2021-01-24 19:54:30 -0300

    Add zombies to desert

--- pickups/key/Key.gd
+++ pickups/key/Key.gd
@@ -1,6 +1,6 @@
 class_name Key extends Pickup
 	
 func body_entered(body: Node2D) -> void:
-	if body.name == 'player' && body.get('keys') < 9:
+	if body.name == 'player' && GameState.keys < 9:
 		body.keys += 1
 		queue_free()
